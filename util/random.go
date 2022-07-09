f8fefc831975c78984e3973d4019371f3b8cc4e8
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-07-09 23:45:59 +0900

    [mod]refactor
    method name change
    if range bigger than needs, not generated using to random
    picked map allocate memory first

--- util/random.go
+++ util/random.go
@@ -10,12 +10,21 @@ import (
 // 必要数に到達していなくても試行回数の限界値を設定しておく
 var limit = math.Pow10(6)
 
-func MakeRandom(min, max, needs int) []int {
+func MakeRandomSlice(min, max, needs int) []int {
 	nRange := max - min
+
+	// 最終系が確定してるので、ランダムで生成しない
+	if nRange < needs {
+		slice := make([]int, nRange)
+		for i := 0; i < nRange; i++ {
+			slice[i] = min + i
+		}
+		return slice
+	}
 
 	rand.Seed(time.Now().UnixMicro())
 
-	picked := make(map[int]bool)
+	picked := make(map[int]bool, needs)
 	for i, limiter := 0, 0; i < needs; {
 		n := rand.Intn(nRange) + min
 		if !picked[n] {
@@ -30,8 +39,10 @@ func MakeRandom(min, max, needs int) []int {
 	}
 
 	res := make([]int, len(picked))
+	k := 0
 	for n := range picked {
-		res = append(res, n)
+		res[k] = n
+		k++
 	}
 
 	return res
