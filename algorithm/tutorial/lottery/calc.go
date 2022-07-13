b666d859cf548e6cbb25d3cc38dceefbc87d3fca
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-07-13 23:59:25 +0900

    [add]lottery algorithm
    minimum test
    full benchmark

---
+++ algorithm/tutorial/lottery/calc.go
@@ -0,0 +1,26 @@
+package lottery
+
+import (
+	"arihon-go/util"
+)
+
+var stack []int
+
+func Calc(n, m int, k []int) bool {
+	stack = make([]int, n*(n+1)/2-1)
+
+	for i := 0; i < n; i++ {
+		stack[i] = k[0] + k[i]
+		if i > 0 {
+			stack[n+i-1] = k[i] + k[n-1]
+		}
+	}
+
+	for kk := range stack {
+		if util.BinarySearch(m-kk, stack) {
+			return true
+		}
+	}
+
+	return false
+}
---
+++ algorithm/tutorial/lottery/calc_test.go
@@ -0,0 +1,42 @@
+package lottery_test
+
+import (
+	"arihon-go/algorithm/tutorial/lottery"
+	"arihon-go/util"
+	"math"
+	"testing"
+)
+
+func TestCalc(t *testing.T) {
+	type args struct {
+		n int
+		m int
+		k []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{name: "simple", args: args{n: 3, m: 10, k: []int{1, 3, 5}}, want: true},
+		// {name: "no answer", args: args{n: 3, m: 9, k: []int{1, 3, 5}}, want: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lottery.Calc(tt.args.n, tt.args.m, tt.args.k); got != tt.want {
+				t.Errorf("Calc() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func BenchmarkCalc(b *testing.B) {
+	nums := util.MakeRandomSlice(1, int(math.Pow10(8)), 1000)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		// this logic answer is `false` = not fast return
+		lottery.Calc(len(nums), 1, nums)
+	}
+}
